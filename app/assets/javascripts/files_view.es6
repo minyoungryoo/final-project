67e49f025f1c730c0b16f04f0a18f32d2b985412
Minyoung Ryoo <minyoung.ryoo@gmail.com>
2016-08-01 14:46:02 -0400

    two graphs working

--- app/assets/javascripts/files_view.es6
+++ app/assets/javascripts/files_view.es6
@@ -14,7 +14,6 @@ $(document).on("turbolinks:load", function() {
 	function financial_data(num_index){
 
 		var content_label = ["Varying Dosages of Hep/Asp", "Heparin", "Aspirin", "Followup to treatment", "Subcutaneous Heparin", "Antiplatelet Drug", "Intravenous Heparin", "Other Anticoagulants", "Calcium Antagonists", "Glycerol or manitol", "Steroids", "Haemodilution", "Carotid Surgery", "Thrombolysis", "Medication Taken at 6-months Followup"];
-		// var finan_data_arr = ["$500","$640", "$383", "$200", "$780", "$320", "$107", "$372", "$6000", "$3000", "$10", "$68", "$15000", "$55", "$200"];
 		var finan_data_arr = [500, 640, 383, 200, 780, 320, 107, 372, 6000, 3000, 10, 68, 15000, 55, 200];
 		var sample_color = ["#BC41EF", "#BB61F3", "#802D7C", "#ECF38B", "#D676D1", "#2F1081", "#AA208A", "#FF6384", "#36A2EB", "#FFCE56", "#BC41EF", "red", "#AA208A", "#AA208A", "#AA208A"];
 
@@ -162,19 +161,16 @@ $(document).on("turbolinks:load", function() {
 
 	function medical_data(response) {
 		var theChart;
-		// if(type == "A"){
-			console.log(response);
-			var data_array = response[1];
-			var error_array = response[2];
-			var arr_length = data_array.length;
-			var color_array = Array(arr_length).fill('rgba(255, 99, 132, 0.2)');
-		// }else{
-		// 	var data_arrayB = response[1];
-		// 	var error_arrayB = response[2];
-		// 	console.log(data_arrayB.length);
-		// 	var arr_length = data_arrayB.length;
-		// 	var color_arrayB = Array(arr_length).fill('blue');
-		// }
+			var data_arrayA = response[0][1];
+			var error_arrayA = response[0][2];
+			var arr_lengthA = data_arrayA.length;
+			var color_arrayA = Array(arr_lengthA).fill('rgba(255, 99, 132, 0.2)');
+
+			var data_arrayB = response[1][1];
+			var error_arrayB = response[1][2];
+			var arr_lengthB = data_arrayB.length;
+			var color_arrayB = Array(arr_lengthB).fill('blue');
+
 
 		var data_label = ["Major Non-cerebral Hemorrhage", "Other Side Effect", "Recurrent Ischemic Stroke", "Recurrent hemorrhagic Stroke", "Recurrent unknown Stroke", "Pulmonary embolism", "Death", "Indicator for hemorrhagic stroke", "Indicator for ischemic stroke","Indicator for indeterminate stroke","Indicator for hemorrhagic transform","Indicator for pulmonary embolism","Indicator for deep vein thrombosis","Indicator for major non-cerebral bleed","Indicator for any stroke"];
 		// 19-25, 30-37
@@ -191,26 +187,26 @@ $(document).on("turbolinks:load", function() {
 		        datasets: [
 			        {
 			            label: '# of Occurances',
-			            data: data_array,
-			            error: error_array,
+			            data: data_arrayA,
+			            error: error_arrayA,
 			            errorDir : "both",
 			            errorStrokeWidth : 1,
 			            errorCapWidth : 0.75,
 			            errorColor: "black",
-			            backgroundColor: color_array,
-			            borderColor: color_array,
+			            backgroundColor: color_arrayA,
+			            borderColor: color_arrayA,
 			            borderWidth: 1
 			        },
 			        {
 			            label: '# of Occurances',
-			            data: data_array,
-			            error: error_array,
+			            data: data_arrayB,
+			            error: error_arrayB,
 			            errorDir : "both",
 			            errorStrokeWidth : 1,
 			            errorCapWidth : 0.75,
 			            errorColor: "black",
-			            backgroundColor: color_array,
-			            borderColor: color_array,
+			            backgroundColor: color_arrayB,
+			            borderColor: color_arrayB,
 			            borderWidth: 1
 			        }
 		        ]
--- app/models/files_analysis.rb
+++ app/models/files_analysis.rb
@@ -1,118 +1,94 @@
 require 'liblinear'
 
 class FilesAnalysis < ApplicationRecord
-	# def doStuff(full_training_array, col_numsA)
 	def doStuff(full_training_array, col_numsA, col_numsB, basic_attribute_array, basic_patient_condition)
-
 		# 19-25, 30-37, 50
 
-		if col_numsA != nil
-
-		full_attribute_array = full_training_array[0]
-		full_attribute_array = full_attribute_array[0..49]
-		
-		attribute_array = basic_attribute_array
-			col_numsA.each do |i|
-				attribute_array = attribute_array.push(full_attribute_array[i.to_i])
+			if col_numsA != nil && col_numsB != nil
+				result_arr_A = runAnalysis(full_training_array, col_numsA, basic_attribute_array, basic_patient_condition)
+				result_arr_B = runAnalysis(full_training_array, col_numsB, basic_attribute_array, basic_patient_condition)
+			elsif col_numsA != nil
+				result_arr_A = runAnalysis(full_training_array, col_numsA, basic_attribute_array, basic_patient_condition)
+				result_arr_B = [0, 0, 0]
+			elsif col_numsB != nil
+				result_arr_A = [0, 0, 0]
+				result_arr_B = runAnalysis(full_training_array, col_numsB, basic_attribute_array, basic_patient_condition)
+			else
+				result_arr_A = [0, 0, 0]
+				result_arr_B = [0, 0, 0]
 			end
 
-
-		full_training_array = full_training_array[1..119]
-		# full_training_array.shift
-
-		# puts "full training_array"
-		# p full_training_array
-		# puts "end **************************"
-		full_training_array.each do |row|
-			row.map! do |element|
-				element.to_i #changed from f
-			end
-		end
-
-		basic_training_array = full_training_array.transpose[0..14]
-		training_array = basic_training_array
-		full_training_array = Matrix[ *full_training_array ]
-			col_numsA.each do |i|
-				tr_cols = full_training_array.column(i.to_i).to_a
-				training_array = training_array.push(tr_cols)
-			end
-		#pushing final decision into array:
-				# 19-25, 30-37, 50
-
-		# patient_condition.fill(0, patient_condition.size, 35)
-		patient_condition = basic_patient_condition.map(&:to_i)
-		col_numsA.each do |i|
-			# patient_condition[i.to_i] = 1
-			patient_condition = patient_condition.push(1)
-		end
-
-		# puts "PAtient condition"
-		# p patient_condition
-		# puts "End **********************"
-
-		full_decision_arr = []
-		mean_result_arr = []
-		std_result_arr = []
-		parameter = { solver_type: Liblinear::L1R_LR  }
-		fold = 3
-		n = -1
-		for i in 19..50
-			if (i >= 19 && i <= 25) || (i >= 30 && i <= 37) || i == 50
-				# i = 50
-				# puts "i is:"
-				# p i
-				# puts "end"
-				# puts "training array before"
-				# p training_array
-				# puts "end *************************"
-				# training_array = training_array.transpose
-				# puts "training array after"
-				# p training_array
-				# puts "End@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-
-				training_array = training_array.push(full_training_array.column(i).to_a)
-				if i == 50
-					dec_tree = DecisionTree::ID3Tree.new(attribute_array, training_array, "4", :continuous)
-				else
-					dec_tree = DecisionTree::ID3Tree.new(attribute_array, training_array, "1", :continuous)
+		def runAnalysis(full_training_array, col_nums, basic_attribute_array, basic_patient_condition)
+			full_attribute_array = full_training_array[0]
+			full_attribute_array = full_attribute_array[0..49]
+			
+			attribute_array = basic_attribute_array
+				col_nums.each do |i|
+					attribute_array = attribute_array.push(full_attribute_array[i.to_i])
 				end
-				dec_tree.train
-				decision = dec_tree.predict(patient_condition)
 
-				# puts "Training array size"
-				# p training_array.transpose.size
-				# puts "end ***********************************"
 
-				attribute_labels = training_array.transpose[n]
-				model = Liblinear.train(parameter, attribute_labels, training_array)
-				lablinear_result = Liblinear.predict(model, patient_condition)
-				crossVal_result = Liblinear.cross_validation(fold, parameter, attribute_labels, training_array).mean
-				full_result = [decision, lablinear_result, crossVal_result]
-				mean_result = full_result.mean
-				std_result = full_result.sd/(Math.sqrt(training_array.length))
-				full_decision_arr = full_decision_arr.push(full_result)
-				mean_result_arr = mean_result_arr.push(mean_result)
-				std_result_arr = std_result_arr.push(std_result)
-				n -= 1
+			full_training_array = full_training_array[1..119]
+			# full_training_array.shift
+
+			full_training_array.each do |row|
+				row.map! do |element|
+					element.to_i #changed from f
+				end
 			end
+
+			basic_training_array = full_training_array.transpose[0..14]
+			training_array = basic_training_array
+			full_training_array = Matrix[ *full_training_array ]
+				col_nums.each do |i|
+					tr_cols = full_training_array.column(i.to_i).to_a
+					training_array = training_array.push(tr_cols)
+				end
+			#pushing final decision into array:
+					# 19-25, 30-37, 50
+
+			# patient_condition.fill(0, patient_condition.size, 35)
+			patient_condition = basic_patient_condition.map(&:to_i)
+			col_nums.each do |i|
+				patient_condition = patient_condition.push(1)
+			end
+
+			full_decision_arr = []
+			mean_result_arr = []
+			std_result_arr = []
+			parameter = { solver_type: Liblinear::L1R_LR  }
+			fold = 3
+			n = -1
+			for i in 19..50
+				if (i >= 19 && i <= 25) || (i >= 30 && i <= 37) || i == 50
+
+					training_array = training_array.push(full_training_array.column(i).to_a)
+					if i == 50
+						dec_tree = DecisionTree::ID3Tree.new(attribute_array, training_array, "4", :continuous)
+					else
+						dec_tree = DecisionTree::ID3Tree.new(attribute_array, training_array, "1", :continuous)
+					end
+					dec_tree.train
+					decision = dec_tree.predict(patient_condition)
+
+					attribute_labels = training_array.transpose[n]
+					model = Liblinear.train(parameter, attribute_labels, training_array)
+					lablinear_result = Liblinear.predict(model, patient_condition)
+					crossVal_result = Liblinear.cross_validation(fold, parameter, attribute_labels, training_array).mean
+					full_result = [decision, lablinear_result, crossVal_result]
+					mean_result = full_result.mean
+					std_result = full_result.sd/(Math.sqrt(training_array.length))
+					full_decision_arr = full_decision_arr.push(full_result)
+					mean_result_arr = mean_result_arr.push(mean_result)
+					std_result_arr = std_result_arr.push(std_result)
+					n -= 1
+				end
+			end
+
+			result = [full_decision_arr, mean_result_arr, std_result_arr]
 		end
 
-		result = [full_decision_arr, mean_result_arr, std_result_arr]
-
-		# puts "==========DECISOIN+++++++++++++++"
-		# p full_decision_arr
-		# p mean_result_arr
-		# p std_result_arr
-		# puts "==========DECISOIN+++++++++++++++"
-
-		# puts "Training arrays all"
-		# p all_training_arr.length
-		# puts "End **********************"
-
-		# training_array = training_array.push(full_training_array.column(50).to_a)
-
-
-
+		final_result_arr = [result_arr_A, result_arr_B]
 
 		# 0-14, 15-18, ... 26-29, 38-49 // not these
 
@@ -219,8 +195,6 @@ class FilesAnalysis < ApplicationRecord
 		# # end
 		# # Statsample::Analysis.run # Open svg file on *nix application defined
 
-		else
-			result = 0
-		end
+
 	end
 end
